ed0a5c490e369a1526131c384b767b7fae9838b1
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-08-04 22:46:45 -0400

    added link to addPost function in MainCtrl. binded to scope.link on main view. added ng-show and hide directives to account for potential missing links

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ angular.module('flapperNews', [])
 			$scope.banana = "I'm a banana!";
 			$scope.addPost = function(){
 				if (!$scope.title || $scope.title ==='') {return}
-				$scope.posts.push({title: $scope.title, upvotes: 0});
+				$scope.posts.push({
+					title: $scope.title,
+					link: $scope.link,
+					upvotes: 0
+				});
+				$scope.title = '';
 				$scope.title = '';
 			};
 			$scope.incrementUpvotes = function(post) {
