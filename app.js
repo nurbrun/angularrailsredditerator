6d6a10a08ee17d7ef6d765b9eb043d72e305a5ff
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-08-04 20:39:24 -0400

    added dummy post data. also added sorting filter to post display

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ angular.module('flapperNews', [])
 	.controller('MainCtrl', [
 		'$scope',
 		function($scope){
-			$scope.test = 'Yo, whaddap!';
+			$scope.posts = [
+			{title: 'Post 1', upvotes: 5},
+			{title: 'Post 2', upvotes: 1},
+			{title: 'Post 3', upvotes: 6},
+			{title: 'Post 4', upvotes: 3},
+			{title: 'Post 5', upvotes: 15},
+			];
 			$scope.banana = "I'm a banana!";
 }]);
